78325de613fc6c444429c4257f14eb60c972f50f
Jeffrey <chenjeff2015@gmail.com>
2021-05-12 13:44:05 -0700

    Complete

--- profInterfaceB.php
+++ profInterfaceB.php
@@ -9,7 +9,8 @@
 	
 	echo ($_POST['c_Num']."-".$_POST['s_Num']."<br>");
 	$query = "SELECT COUNT(*) as total FROM Section S, Enrollment_Records E WHERE S.section_Num = '".$_POST['c_Num']."-".$_POST['s_Num']."' AND E.grade = 'A' AND E.sec_Num = S.section_Num";
-    $result = $link->query($query);
+    
+	$result = $link->query($query);
 	$row = $result->fetch_assoc();
 	echo ("A: ".$row[total]."<br>");
 	$result->free_result();
