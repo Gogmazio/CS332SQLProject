48b384c51251f0dc7af69bd9e7c697320b33a183
Jeffrey <chenjeff2015@gmail.com>
2021-05-12 13:35:40 -0700

    Complete
    Adjusted Indentations

--- stuInterfaceA.php
+++ stuInterfaceA.php
@@ -1,28 +1,28 @@
 <html>
-	<body>
-		<?php
-			$link = mysqli_connect('mariadb', 'username', 'password', 'dbname');
-			if (!$link) {
-			die('Could not connect: '.mysql_error());
-			}
-			echo "Connected successfully<p>";
-			
-			$query = "SELECT S.section_Num, S.classroom, S.meeting_Day, S.start_Time, S.end_Time, S.number_Of_Seats FROM Section S, Course C WHERE S.cour_Num = C.course_Num AND C.course_Num = '".$_POST['c_Num']."'";
-			$result = $link->query($query);
-			$nor = $result->num_rows;
+<body>
+<?php
+	$link = mysqli_connect('hostname', 'username', 'password', 'dbname');
+	if (!$link) {
+	die('Could not connect: '.mysql_error());
+	}
+	echo "Connected successfully<p>";
+		
+	$query = "SELECT S.section_Num, S.classroom, S.meeting_Day, S.start_Time, S.end_Time, S.number_Of_Seats FROM Section S, Course C WHERE S.cour_Num = C.course_Num AND C.course_Num = '".$_POST['c_Num']."'";
+	$result = $link->query($query);
+	$nor = $result->num_rows;
 	
-			for($i=0;$i<$nor;$i++){
-				$row = $result->fetch_assoc();
-				echo $row["section_Num"]." ";
-				echo $row["classroom"]." ";
-				echo $row["meeting_Day"]." ";
-				echo $row["start_Time"]." ";
-				echo $row["end_Time"]." ";
-				echo $row["number_Of_Seats"]."<br>";
-			}
+	for($i=0;$i<$nor;$i++){
+		$row = $result->fetch_assoc();
+		echo $row["section_Num"]." ";
+		echo $row["classroom"]." ";
+		echo $row["meeting_Day"]." ";
+		echo $row["start_Time"]." ";
+		echo $row["end_Time"]." ";
+		echo $row["number_Of_Seats"]."<br>";
+	}
 
-			$result->free_result();
-			$link->close();
-		?>
-	</body>
+	$result->free_result();
+	$link->close();
+?>
+</body>
 </html>
